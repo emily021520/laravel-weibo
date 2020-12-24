cb8efb7f5e13cff0534695814c10cf63e147b2b0
emily021520 <985133556@qq.com>
2020-12-24 17:59:42 +0800

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,4 +18,23 @@ class UsersController extends Controller
         //compact() 函数创建一个包含变量名和它们的值的数组,并作为第二个参数传递给view方法，将数据与视图进行绑定，这样子就可以得到数据库的信息啦。
         return view('users.show',compact('user'));
     }
+
+
+    /**
+     * 验证规则
+     * required 必须要填
+     * unique 唯一性
+     * min|max 长度验证
+     * email 格式验证
+     * confirmed 密码匹配验证 保证两次输入的密码一致
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,28 +9,38 @@
     </div>
     <div class="card-body">
       <form method="POST" action="{{ route('users.store') }}">
-          <div class="form-group">
-            {{-- 辅助函数 old 来帮助我们在 Blade 模板中显示旧输入数据 --}}
-            <label for="name">名称：</label>
-            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
-          </div>
+        {{ csrf_field() }}
 
-          <div class="form-group">
-            <label for="email">邮箱：</label>
-            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
-          </div>
+        {{--
+          经过POST方法提交时，Laravel 为了安全考虑，会让我们提供一个 token（令牌）来防止我们的应用受到 CSRF（跨站请求伪造）的攻击。
 
-          <div class="form-group">
-            <label for="password">密码：</label>
-            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
-          </div>
+          上方调用如 <input type="hidden" name="_token" value="fhcxqT67dNowMoWsAHGGPJOAWJn8x5R5ctSwZrAq">
 
-          <div class="form-group">
-            <label for="password_confirmation">确认密码：</label>
-            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
-          </div>
+          CSRF 令牌基于会话（Session），过期时间在 config/session.php 文件中的 lifetime 选项做设定，默认为 2 个小时。
+        --}}
 
-          <button type="submit" class="btn btn-primary">注册</button>
+
+        <div class="form-group">
+          <label for="name">名称：</label>
+          <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="email">邮箱：</label>
+          <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="password">密码：</label>
+          <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="password_confirmation">确认密码：</label>
+          <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">注册</button>
       </form>
     </div>
   </div>
