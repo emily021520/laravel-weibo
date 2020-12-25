34aa8c5d30a62629db71e2013837ef77df411d38
emily021520 <985133556@qq.com>
2020-12-25 15:49:27 +0800

    登录时记住我功能

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -25,12 +25,19 @@ class SessionsController extends Controller
      */
     public function store(Request $request)
     {
+
+        /**
+         * 验证
+         */
         $credentials = $this->validate($request, [
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($credentials)) {
+        /**
+         * attempt()第一个参数是进行用户身份认证的数组，第二个参数为是否为用户开启【记住我】功能的布尔值，括号里面的值是表单里的name值
+         */
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
             return redirect()->route('users.show', [Auth::user()]);
         } else {
@@ -49,6 +56,7 @@ class SessionsController extends Controller
         session()->flash('success','您已成功退出！');
         return redirect('login'); //并跳转到登录页面
     }
+
 
 
 
